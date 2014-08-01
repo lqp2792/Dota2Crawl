5d5e3152ccb9f5e82e7fe4a38ee0f62d719a23c9
phu <phu@phu-MS-7623>
2014-08-02 06:08:09 +0700

    abc

--- src/main/java/phu/quang/le/SimpleVisitor/SimpleVisitor.java
+++ src/main/java/phu/quang/le/SimpleVisitor/SimpleVisitor.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 
 import org.htmlparser.Tag;
 import org.htmlparser.Text;
+import org.htmlparser.tags.LinkTag;
 import org.htmlparser.tags.TableColumn;
 import org.htmlparser.tags.TableRow;
 import org.htmlparser.visitors.NodeVisitor;
@@ -27,18 +28,19 @@ public class SimpleVisitor extends NodeVisitor {
 			String presentText = null;
 			StringTokenizer tokens = null;
 			for(TableColumn tc : tcs) {
-				System.out.println("\n" + tc.getStringText());
-				System.out.println(Jsoup.parse(tc.getStringText()).text());
 				presentText = Jsoup.parse(tc.getStringText()).text();
 				if(prevText != null) {
 					switch(prevText) {
 					case "Movement Speed":
+						System.out.println("Set Movement Speed: " + presentText);
 						heroStat.setMoveSpeed(Integer.parseInt(presentText));
 						break;
 					case "Turn Rate":
+						System.out.println("Set Turn rate: " + presentText);
 						heroStat.setTurnRate(Double.parseDouble(presentText));
 						break;
 					case "Sight Range":
+						System.out.println("Set Sight range: " + presentText);
 						tokens = new StringTokenizer(presentText, "/");
 						if(tokens.hasMoreTokens()) {
 							heroStat.setDaySightRange(Integer.parseInt(tokens.nextToken()));
@@ -46,12 +48,15 @@ public class SimpleVisitor extends NodeVisitor {
 						}
 						break;
 					case "Attack Range":
+						System.out.println("Set Attack range: " + presentText);
 						heroStat.setMissileSpeed(presentText);
 						break;
 					case "Missile Speed":
+						System.out.println("Set Missile Speed: " + presentText);
 						heroStat.setMissileSpeed(presentText);
 						break;
 					case "Attack Duration":
+						System.out.println("Set Attack Duration: " + presentText);
 						tokens = new StringTokenizer(presentText, "+");
 						if(tokens.hasMoreTokens()) {
 							heroStat.setAttackPoint(Double.parseDouble(tokens.nextToken()));
@@ -59,6 +64,7 @@ public class SimpleVisitor extends NodeVisitor {
 						}
 						break;
 					case "Cast Duration": 
+						System.out.println("Set Cast Duration: " + presentText);
 						tokens = new StringTokenizer(presentText, "+");
 						if(tokens.hasMoreTokens()) {
 							heroStat.setCastPoint(Double.parseDouble(tokens.nextToken()));
@@ -66,12 +72,16 @@ public class SimpleVisitor extends NodeVisitor {
 						}
 						break;
 					case "Base Attack Time": 
+						System.out.println("Set Base Attack Time: " + presentText);
 						heroStat.setBaseAttackTime(Double.parseDouble(presentText));
 						break;
 					}
 				}
 				prevText = presentText;
 			}
+		} else if (tag instanceof LinkTag) {
+			LinkTag lt = (LinkTag) tag;
+			System.out.println(Jsoup.parse(lt.getStringText()).text());
 		}
 	}
 	
